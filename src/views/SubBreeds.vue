8cb5f3e9d9eaf8262430b5864f71c9ebe7e41979
Aisha Keita <aishakeita6@gmail.com>
2021-02-20 10:04:40 +0100

    subBreeds.vue
    - import footer component

--- src/views/SubBreeds.vue
+++ src/views/SubBreeds.vue
@@ -2,12 +2,17 @@
 <template>
   <div class="sub-Breeds">
     <h1>welcome to the sub-breeds</h1>
+      <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '../components/Footer'
 export default {
   name: 'SubBreeds',
+  components: {
+    Footer
+  },
   data () {
     return {
     }
