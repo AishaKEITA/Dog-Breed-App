2dd213c8ec5c11a311437b4c0508ab4b7d258f9b
Aisha Keita <aishakeita6@gmail.com>
2021-02-20 09:56:39 +0100

    breeds.vue
    - import footer component

--- src/views/Breeds.vue
+++ src/views/Breeds.vue
@@ -2,12 +2,17 @@
 <template>
   <div class="breeds">
     <h1>welcome to the breeds</h1>
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '../components/Footer'
 export default {
   name: 'Breeds',
+  components: {
+    Footer
+  },
   data () {
     return {
     }
