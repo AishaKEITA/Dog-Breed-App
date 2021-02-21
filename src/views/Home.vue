3dd9811785a7e1e1e3116cc637f934def89803a9
Aisha Keita <aishakeita6@gmail.com>
2021-02-21 23:39:54 +0100

    home.vue
    - add get randome image api
    - add get random image function
    - add button to get random image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,8 @@
   <div class="home">
     <v-app>
       <h1>{{ welcomeMessage }}</h1>
+   <v-btn color="#264653" class="text-xs-center" @click="getRandomImage()">{{ randomImageButton }}</v-btn>
+   <img v-bind:src='randomImage'/>
       <Footer />
     </v-app>
   </div>
@@ -17,15 +19,32 @@ export default {
   },
   data () {
     return {
-      welcomeMessage: 'Welcome to the land of dogs'
+      welcomeMessage: 'Welcome to the land of dogs',
+      randomImageButton: 'Click random Imagae',
+      randomImage: ''
     }
   },
 
   /* --- simple fetch of the api --- */
   created () {
-    fetch('https://dog.ceo/api/breeds/list/all')
-      .then(response => response.json())
-      .then(data => console.log(data))
+
+  },
+
+  methods: {
+    /* --- fetch random image --- */
+    getAllBreedList () {
+      fetch('https://dog.ceo/api/breeds/list/all')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    },
+    getRandomImage () {
+      fetch('https://dog.ceo/api/breeds/image/random')
+        .then(response => response.json())
+        .then(data => {
+          this.randomImage = data.message
+          console.log(this.randomImage)
+        })
+    }
   }
 }
 </script>
@@ -34,5 +53,8 @@ h1 {
   font-size: 50px;
   text-align: center;
   color: #6d6875;
+}
+.home {
+  height: auto
 }
 </style>
