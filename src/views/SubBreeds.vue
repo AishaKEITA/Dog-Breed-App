4d7dfcb3eac6ce4e07f9aa5e5ef369943ba4ca05
Aisha Keita <aishakeita6@gmail.com>
2021-02-25 12:46:40 +0100

    - fetch get all breed list

--- src/views/SubBreeds.vue
+++ src/views/SubBreeds.vue
@@ -2,19 +2,49 @@
 <template>
   <div class="sub-Breeds">
     <h1>Check out breeds and sub-breeds</h1>
+      <div id="subbreeds">
+      <Breed
+        v-for="(breedName, index) in breedNames"
+        :key="index"
+        :name="breedName"
+      />
+    </div>
+
       <Footer />
   </div>
 </template>
 
 <script>
 import Footer from '../components/Footer'
+import Breed from './Breed'
 export default {
     name: 'SubBreeds',
     components: {
-        Footer
+        Footer,
+        Breed
     },
     data () {
         return {
+            breedData: '',
+            breedNames: []
+        }
+    },
+    created () {
+        this.getBreedList()
+    },
+    methods: {
+        /* --- fetch breeds all breeds --- */
+        getBreedList () {
+            const temporaryBreedNames = []
+            fetch('https://dog.ceo/api/breeds/list/all')
+                .then(response => response.json())
+                .then(data => {
+                    this.breedData = data.message
+                    for (let breed in this.breedData) {
+                        temporaryBreedNames.push(breed)
+                    }
+                    this.breedNames = temporaryBreedNames
+                })
         }
     }
 }
