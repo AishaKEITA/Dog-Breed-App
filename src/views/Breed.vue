13a9079f3dd04023ff214c05cfc4453d1bc95841
Aisha Keita <aishakeita6@gmail.com>
2021-02-25 11:26:02 +0100

    eslintrc.js
    - add eslintrc rule

---
+++ src/views/Breed.vue
@@ -0,0 +1,78 @@
+
+<template>
+  <div class="breedbtn">
+    <v-btn
+      class="white--text"
+      color="#264653"
+      width="200px"
+      @click="breedClickHandler()"
+    >
+      {{ name }}
+    </v-btn>
+    <div class="breedImage" v-if="showbreedImages">
+      <img :src="breedImages[imageIndex]" height="300" width="auto" />
+      <div>
+        <v-btn
+          class="white--text"
+          color="#fcbf49"
+          width="200px"
+          @click="incrementIndex()"
+        >
+          {{ newImageText }}
+        </v-btn>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+<script>
+export default {
+    name: 'Breed',
+    props: {
+        name: {
+            type: String,
+            required: true
+        }
+    },
+    data () {
+        return {
+            breedImages: [],
+            imageIndex: 0,
+            showbreedImages: false,
+            needFetch: true,
+            newImageText: 'New image'
+        }
+    },
+    methods: {
+    /* --- fetch breeds by image --- */
+        getBreedImages () {
+            fetch(`https://dog.ceo/api/breed/${this.name}/images`)
+                .then(response => response.json())
+                .then(data => {
+                    this.breedImages = data.message.slice(0, 8)
+                })
+            console.log(this.breedImages)
+        },
+        /* --- handle click of getting breed image--- */
+        breedClickHandler () {
+            if (this.needFetch) {
+                this.getBreedImages()
+                this.needFetch = false
+            }
+            this.showbreedImages = !this.showbreedImages
+        },
+        /* --- handle increment for each click --- */
+        incrementIndex () {
+            this.imageIndex++
+            if (this.imageIndex >= this.breedImages.length) {
+                this.imageIndex = 0
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+.breedImage {
+  display: block;
+}
+</style>
